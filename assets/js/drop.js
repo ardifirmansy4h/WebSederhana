33ee6b144f675c53c3fe055ceed1a825989b74e2
ardifirmansy4h <ardifirmansy4h.com>
2022-06-21 17:10:39 +0700

    first commit

---
+++ assets/js/drop.js
@@ -0,0 +1,14 @@
+function dropF() {
+    document.getElementById("dropDown").classList.toggle("turun");
+  }
+  
+function dropA() {
+    document.getElementById("dropDown1").classList.toggle("turun");
+  }
+function dropZ() {
+    document.getElementById("dropDown2").classList.toggle("turun");
+  }
+function dropX() {
+    document.getElementById("dropDown3").classList.toggle("turun");
+  }
+  
